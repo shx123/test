f0767f37b475955a777a2b707ee7b52366de98e6
sunhuixia <252919324@qq.com>
2017-11-28 14:51:21 +0800

    'test.php'

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+/*	if($_GET['url']=='abc'){
+		echo 'abc';
+	}else{
+		echo '木有';
+	} 
+
+		
+	if($_POST['name']=='abc'){
+		echo 'abc';
+	}else{
+		echo '木有';
+	} 
+*/
+echo $_POST['user'].'-'.$_POST['email'];
+
+
+
+
+?>
